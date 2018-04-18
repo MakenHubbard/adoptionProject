5dd09c59349bab16b921179bddc1fdfe45958a50
Maken Hubbard <makhub26@outlook.com>
2018-04-18 18:01:54 -0500

    I have the styling finished

--- javascripts/event.js
+++ javascripts/event.js
@@ -2,20 +2,22 @@ const whatINeed = require('./data');
 const printToDom = require('./dom');
 
 const addEvents = () => {
-  console.log('add events is running');
   document.getElementById('cat').addEventListener('click', sortPets);
   document.getElementById('dog').addEventListener('click', sortPets);
   document.getElementById('dino').addEventListener('click', sortPets);
+  document.getElementById('refresh').addEventListener('click', refreshButtonEvents);
 };
 
 const sortPets = (e) => {
   const clickedType = e.target.id;
   const allPetsArray = whatINeed.gotDemPets();
-  console.log(allPetsArray);
-  console.log('petType', clickedType);
   const selectedPets = allPetsArray.filter(pet => pet.type === clickedType);
-  console.log(selectedPets);
   printToDom(selectedPets);
+};
+
+const refreshButtonEvents = () => {
+  const bringAllPets = whatINeed.gotDemPets();
+  printToDom(bringAllPets);
 };
 
 module.exports = addEvents;
