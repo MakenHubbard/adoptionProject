aee36128b4bd7965c7e112d5fd0c7a92432dcd3c
Maken Hubbard <makhub26@outlook.com>
2018-04-18 16:32:11 -0500

    adding some comments explaining line by line

--- javascripts/event.js
+++ javascripts/event.js
@@ -1,13 +1,6 @@
 const whatINeed = require('./data');
 const printToDom = require('./dom');
 
-// const petCards = document.getElementByClassName('pets');
-
-// const showType = (e) => {
-//   const petType = e.target;
-//   petType.classList.add('');
-// };
-
 const addEvents = () => {
   console.log('add events is running');
   document.getElementById('cat').addEventListener('click', sortPets);
